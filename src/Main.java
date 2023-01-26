2e3f450818f1df922ec08cc0044950cc4081fdf6
ernazaresanbekov <ernazar.03@icloud.com>
2023-01-26 16:14:27 +0600

    Homework10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(contains(scanner.nextInt(), 2, 3, 8, 22, 34, 65, 59, 43));
+
+    }static boolean contains (int a ,int...varargs) {
+        boolean n = true;
+        for (int b: varargs) {
+            if (a==b){
+                return n;
+
+            }
+        }return false;
     }
 }
